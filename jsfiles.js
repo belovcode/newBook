df6fb2cb3c2e5cb903f818c5c6ec14ec8a273865
Mudreishii <Mudreishii@yandex.ru>
2014-04-09 11:27:20 +0300

    mod1

--- jsfiles.js
+++ jsfiles.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
 
+	if(!localStorage.getItem("Contacts")){
+			var Contact = [];
+			localStorage.setItem("Contacts",JSON.stringify(Contact));
+		};
+		var Contacts = JSON.parse(localStorage.getItem("Contacts"));
+
+	// Вывод контактов
+	
+	shouContacts();
+
 							/* Вывод контактов, function */
 	function shouContacts () {
 		if (!Contacts[0]) {
@@ -10,22 +20,24 @@ $(document).ready(function () {
 		$('.main tbody tr').remove();
 		
 		$.each(Contacts, function (ind, value) {
-			var firstName = value.firstName;
-			var lastName = value.lastName;
-			var phoneNumber = value.phoneNumber;
-			
-			var redDelBl = $('<div>').addClass('redDelImg');
-			var img1 = $("<img src='img/karandash.png' alt='red' width='20' height='20'>").addClass('redactContact');
-			var img2 = $("<img src='img/vedro.png' alt='del' width='20' height='20'>").addClass('deleteContact');
-			redDelBl.append(img1, img2);
+			if (value.visible) {
+				var firstName = value.firstName;
+				var lastName = value.lastName;
+				var phoneNumber = value.phoneNumber;
+				
+				var redDelBl = $('<div>').addClass('redDelImg');
+				var img1 = $("<img src='img/karandash.png' alt='red' width='20' height='20'>").addClass('redactContact');
+				var img2 = $("<img src='img/vedro.png' alt='del' width='20' height='20'>").addClass('deleteContact');
+				redDelBl.append(img1, img2);
 
-			var tr = $('<tr>').data("index",ind);
-			$('<td>').addClass('numberContact').text(ind+1).appendTo(tr);
-			$('<td>').text(firstName).appendTo(tr);
-			$('<td>').text(lastName).appendTo(tr);
-			$('<td>').text(phoneNumber).appendTo(tr);
-			$('<td>').append(redDelBl).appendTo(tr);
-			tr.appendTo($('tbody'));
+				var tr = $('<tr>').data("index",ind);
+				$('<td>').addClass('numberContact').text(ind+1).appendTo(tr);
+				$('<td>').text(firstName).appendTo(tr);
+				$('<td>').text(lastName).appendTo(tr);
+				$('<td>').text(phoneNumber).appendTo(tr);
+				$('<td>').append(redDelBl).appendTo(tr);
+				tr.appendTo($('tbody'));
+			};
 			console.log(Contacts);
 		});
 	};
@@ -65,17 +77,13 @@ $(document).ready(function () {
 		Contacts[numberContact].firstName = firstName;
 		Contacts[numberContact].lastName = lastName;
 		Contacts[numberContact].phoneNumber = phoneNumber;
+		localStorage.setItem("Contacts", JSON.stringify(Contacts));
 
 		$('.novContact').text("New contact");
 		$('input[name=submit]').val('Add Contact');
 		$('input[name=submit]').addClass('ok').removeClass('redactButton');
 		shouContacts();
 	};
-
-	// Вывод контактов
-
-	var Contacts = [];
-	shouContacts();
 
 	// Открытие формы добовления контакта
 
@@ -97,12 +105,14 @@ $(document).ready(function () {
 			var contact = {
 				firstName: firstName,
 				lastName: lastName,
-				phoneNumber: phoneNumber
+				phoneNumber: phoneNumber,
+				visible: true
 			};
 			$('input[type=text]').css({
 				'backgroundColor':'white'
 			});
 			Contacts.push(contact);
+			localStorage.setItem("Contacts", JSON.stringify(Contacts));
 			closeShouForm(); /*Закрытие формы*/
 			shouContacts(); /*Вывод всех контактов*/
 		} else {
@@ -176,16 +186,75 @@ $(document).ready(function () {
 		var tr = $(this).closest('tr');
 		var numberContact = tr.data("index");
 		Contacts.splice(numberContact,1);
+		localStorage.setItem("Contacts", JSON.stringify(Contacts));
 		tr.remove();
 		shouContacts(); /*Вывод всех контактов*/
+	});
+
+	// Поиск
+
+	$('input[name=searchVal]').keyup(function () {
+		var searchValue = $(this).val();
+
+		$.each(Contacts, function (index, contact) {
+			var name = contact.firstName;
+			if (contact.firstName.indexOf(searchValue) >= 0 || contact.lastName.indexOf(searchValue) >= 0 || contact.phoneNumber.indexOf(searchValue) >= 0) {
+				contact.visible = true;
+			} else {
+				contact.visible = false;
+			};
+			shouContacts();
+		})
 	})
 
+	// Отправка значений в localStorage
 
+		// Без передачи переменной
+			localStorage.setItem("name","Dima"); /* setItem - Передаёт значения */
+			var a = localStorage.getItem("name"); /* getItem - Возвращает значения */
+			// var a = localStorage.name; Одно и тоже
+			// alert(a);
 
+		// С передачей переменной
+			var b = "OLOLO";
+			localStorage.setItem("name",b); /*Задаём ключ name и передаём значение OLOLO*/
+			var b1 = localStorage.getItem("name");
+			// var a = localStorage.name; Одно и тоже
+			// alert(b1);
 
+		// Передача массивов ( кодируем данныи )
+			var mas1 = {/*Асоциативный массив*/ 
+				value1: "a",
+				value2: "b",
+				value3: "c"
+			};
+			localStorage.setItem("data1", JSON.stringify(mas1));
+			var c1 = JSON.parse(localStorage.getItem("data1"));
+			// alert(c1.value2);
 
+			var mas2 = ['A','B','C'];/*Простой массив*/
+			localStorage.setItem("data2", JSON.stringify(mas2));
+			var c2 = JSON.parse(localStorage.getItem("data2"));
+			// alert(c2[1]);
 
+			var mas3 = [/*Сложный массив*/
+				{
+					value1: "a",
+					value2: "b",
+					value3: "c"
+				},
+				{
+					value4: "e",
+					value5: "f",
+					value6: "j"
+				}];
+			localStorage.setItem("data3", JSON.stringify(mas3));
+			var c3 = JSON.parse(localStorage.getItem("data3"));
+			// alert(c3[1].value5);
 
+		// Удоление из Хранилища
+			localStorage.removeItem("data");/*Удоление элем. из хранилища*/
+			// localStorage.clear(); Полная очистка хранилища
 
 
 	$('#addNewContacts').css({
